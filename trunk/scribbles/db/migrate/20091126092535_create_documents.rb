ecbd78a8876eb6129e5ec25c94a52047afcb6319
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-05 09:03:14 +0000

    Added relationships to accounts and documents

--- trunk/scribbles/db/migrate/20091126092535_create_documents.rb
+++ trunk/scribbles/db/migrate/20091126092535_create_documents.rb
@@ -1,8 +1,9 @@
 class CreateDocuments < ActiveRecord::Migration
   def self.up
     create_table :documents do |t|
-      t.string :name
-      t.text :body
+      t.string  :name
+      t.text    :body
+      t.integer :account_id
 
       t.timestamps
     end
