d0bc8029a40f1eef808148f21f4b5e08380bd873
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-05 08:13:53 +0000

    Login form now creates a user session. Next step: login/registration sends user to user's home page

--- scribbles/app/controllers/home_controller.rb
+++ scribbles/app/controllers/home_controller.rb
@@ -7,6 +7,11 @@ class HomeController < ApplicationController
   # Login
   def login
     flash.now[:pw_warning] = "Wrong username or password"
+    @current_user = Account.find_by_name_and_password(params[:username], params[:password])
+    if @current_user
+      session[:user_id] = @current_user.id
+      flash.now[:pw_warning] = @current_user.name
+    end
   end
 
 end
