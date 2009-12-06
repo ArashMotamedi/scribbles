a56d43e424c8959dc09248bc56ab78d58b05520a
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-06 08:07:31 +0000

    Added documents page (our main page where you can enter text and stuff). Login now goes to the main page. The page currently only has a banner at the top.

--- scribbles/app/controllers/documents_controller.rb
+++ scribbles/app/controllers/documents_controller.rb
@@ -1,18 +1,18 @@
-class HomeController < ApplicationController
-
-  # Main landing page
+class DocumentsController < ApplicationController
   def index
   end
-
-  # Login
+  
   def login
     @current_user = Account.find_by_name_and_password(params[:username], params[:password])
     if @current_user
       # Log user in
       session[:user_id] = @current_user.id
     else
-      flash.now[:pw_warning] = "Wrong username or password"
+      flash.now[:error] = "Wrong username or password"
     end
   end
-
+  
+  def logout
+    session[:user_id] = @current_user = nil
+  end
 end
