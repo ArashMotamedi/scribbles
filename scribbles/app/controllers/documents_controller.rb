a027100983b65c7c6c9fa02017c383991ab81f15
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-07 08:26:42 +0000

    Going to a URL such as http://localhost:3000/myname creates a temporary account with a default doc

--- scribbles/app/controllers/documents_controller.rb
+++ scribbles/app/controllers/documents_controller.rb
@@ -1,7 +1,22 @@
 class DocumentsController < ApplicationController
   before_filter :get_logged_in_user
   
+  # Variables
+  # @current_user - The currently logged in user
+  # @this_doc - The document that is displayed on the page
+  
   def index
+    # Get the account of this page
+    accountOfThisPage = Account.find_by_name(params[:pagename])
+    
+    # If this page's account exists, get the default doc for the account
+    if accountOfThisPage
+      @this_doc = Document.find_by_account_id_and_name(accountOfThisPage.id, "default")
+    # Else, create a new temporary account and default doc
+    else
+      # Method creates a temp account and returns the default doc
+      @this_doc = create_temp_account(params[:pagename])
+    end
   end
   
   def login
@@ -16,6 +31,29 @@ class DocumentsController < ApplicationController
   
   def logout
     session[:user_id] = @current_user = nil
+  end
+  
+  #### Private methods below ####
+  private
+  
+  # Creates a new temporary account and returns the default doc for the account
+  def create_temp_account(name)
+    # Create the new account
+    acc = Account.new
+    acc.name = name
+    acc.expiration = 7;
+    acc.is_permanent = false
+    acc.save!
+    
+    # Create the default doc for the account
+    newAcc = Account.find_by_name(name)
+    doc = Document.new
+    doc.name = "default"
+    doc.account_id = newAcc.id
+    doc.body = ""
+    doc.save!
+    
+    return doc
   end
 
 end
--- scribbles/app/models/account.rb
+++ scribbles/app/models/account.rb
@@ -3,13 +3,13 @@ class Account < ActiveRecord::Base
 
   # Validation
   validates_presence_of :name
-  validates_presence_of :is_permanent
-  if :is_permanent
+  validates_presence_of :expiration
+  validates_inclusion_of :is_permanent, :in => [true, false]
+  if :is_permanent == true
     validates_presence_of :password
     validates_presence_of :email
+    validates_format_of :email, :with => /\A[\w\.]+@[\w\.]+\Z/, :message => "has invalid format"
   end
   
   validates_uniqueness_of :name
-  
-  validates_format_of :email, :with => /\A[\w\.]+@[\w\.]+\Z/, :message => "has invalid format"
 end
