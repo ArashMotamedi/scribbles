0579f5b64c5d3cee2b81db9ee0e03363c3933bc4
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-08 22:20:23 +0000

    Integrated the documents interface with the Rails project. Had to move a bunch of things around and make some changes to the docs to eliminate all the errors. Arash, let me know if these changes affect you. Still need to make some more changes to the documents controller code, will save for next rev.

---
+++ trunk/scribbles/public/javascripts/Upload.js
@@ -0,0 +1,15 @@
+﻿function beginUpload() {
+	// Write file name in the progress
+	var fileName = document.getElementById("file").value;
+	if (fileName == null || fileName == "") {
+		return false;
+	}
+	fileName = fileName.substring(fileName.lastIndexOf("\\"));
+
+	document.getElementById("div_progress").innerHTML = fileName;
+	
+	document.getElementById("div_form").style.visibility = "hidden";
+	document.getElementById("div_progress").style.visibility = "visible";
+
+	return true;
+}
