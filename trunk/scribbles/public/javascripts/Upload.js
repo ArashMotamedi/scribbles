31898652cc2ad45172a4a57b99e6dc6758f5d46b
arash.motamedi <arash.motamedi@143695d2-d35d-11de-901b-a378632c6355>
2009-12-11 06:36:15 +0000

    Upload javascript

--- trunk/scribbles/public/javascripts/Upload.js
+++ trunk/scribbles/public/javascripts/Upload.js
@@ -1,4 +1,4 @@
-﻿function beginUpload() {
+﻿function beginUpload(docId, description) {
 	// Write file name in the progress
 	var fileName = document.getElementById("file").value;
 	if (fileName == null || fileName == "") {
@@ -6,10 +6,17 @@
 	}
 	fileName = fileName.substring(fileName.lastIndexOf("\\"));
 
+	// Add Description
+	document.getElementById("description").value = description;
+
 	document.getElementById("div_progress").innerHTML = fileName;
 	
 	document.getElementById("div_form").style.visibility = "hidden";
 	document.getElementById("div_progress").style.visibility = "visible";
-
+	
+	// Submit the form
+	document.forms[0].action = "documents/UploadFile/" + docId;
+	document.forms[0].submit();
+	
 	return true;
 }
