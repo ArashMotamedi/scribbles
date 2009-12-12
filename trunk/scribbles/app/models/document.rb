730999adaeb77f3a387f3a01584cca8087fabe55
linhnvo <linhnvo@143695d2-d35d-11de-901b-a378632c6355>
2009-12-12 11:17:54 +0000

    Added Password.html.erb. It invokes the action /documents/SetPassword. Please re-migrate for this revision.

--- trunk/scribbles/app/models/document.rb
+++ trunk/scribbles/app/models/document.rb
@@ -3,4 +3,48 @@ class Document < ActiveRecord::Base
   has_many :comments
   has_many :logs
   has_many :file_tabs
+  
+  # Validation
+  validates_presence_of :name, :account_id
+  
+  attr_protected :id, :salt
+  attr_accessor :password
+  
+  def self.authenticate(docid, pw)
+    doc = find(:first, :conditions => {:id => docid})
+    if doc.nil?
+      return nil
+    end
+    
+    if Document.encrypt(pw, doc.salt) == doc.secure_password
+      return doc
+    end
+
+    nil
+  end
+  
+  def password=(pw)
+    @password = pw
+    if not self.salt?
+      self.salt = Document.random_str(10)
+    end
+    
+    self.secure_password = Document.encrypt(@password, self.salt)
+  end
+  
+  #### Protected methods ####
+  protected
+  
+  def self.encrypt(pw, salt)
+    Digest::SHA1.hexdigest(pw + salt)
+  end
+  
+  def self.random_str(length)
+    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    rnd = ""
+    1.upto(length) do |i|
+      rnd << chars[rand(chars.size-1)]
+    end
+    rnd
+  end
 end
